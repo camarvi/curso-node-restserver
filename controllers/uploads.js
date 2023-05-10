76fce868c0734b8405b03f6ca0aa6ef16f886f74
kamarvi <camarvi24@gmail.com>
2023-05-10 21:34:47 +0200

    subir ficheros

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,11 +1,36 @@
+const path = require('path');
+
 const { response } = require("express");
 
 
 const cargarArchivo = (req, res = response) => {
 
-    res.json({
-        msg: 'Ruta cargar Archivo'
-    })
+    //let sampleFile;
+    //let uploadPath;
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+        res.status(400).json({ msg: 'No hay archivos que subir' });
+        return;
+    }
+
+    if (!req.files.archivo) {
+        res.status(400).json({ msg: 'No hay archivos que subir' });
+        return;
+    }
+
+
+    //sampleFile = req.files.sampleFile;
+    const { archivo } = req.files;
+
+    const uploadPath = path.join(__dirname, '../uploads/', archivo.name);
+
+    archivo.mv(uploadPath, (err) => {
+        if (err) {
+            return res.status(500).json({ err });
+        }
+
+        res.json({ msg: 'File uploaded to ' + uploadPath });
+    });
 
 }
 
--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors')
+const fileUpload = require('express-fileupload');
 
 const { dbConnection } = require('../database/config');
 
@@ -46,7 +47,14 @@ class Server {
         this.app.use(express.json());
 
         // Directorio Publico
-        this.app.use(express.static('public'))
+        this.app.use(express.static('public'));
+
+        // Carga de archivos. 
+        this.app.use(fileUpload({
+            useTempFiles: true,
+            tempFileDir: '/tmp/'
+        }));
+
     }
 
     routes() {
